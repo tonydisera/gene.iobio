7562b2cb1c0ae5f30e61d34761b19ec376581944
tonydisera <tony.disera@gmail.com>
2015-08-04 10:17:17 -0600

    Warning when gene.iobio is run from IE browser.

--- app/app.js
+++ app/app.js
@@ -74,6 +74,10 @@ var pageGuide = null;
 
 $(document).ready(function(){
 
+	if (detectIE() == true) {
+		alert("Warning. Gene.iobio has been tested and verified on Chrome, Firefox, and Safari browsers.  Please run gene.iobio from one of these browsers.");
+	}
+
 	// Compile handlebar templates, when all are loaded
 	// call init();
 	$.get('templates/dataCardEntryTemplate.hbs', function (data) {
@@ -208,6 +212,37 @@ function init() {
 
 	loadGeneFromUrl();
 }
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // IE 12 => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 
 function getProbandVariantCard() {
 	var probandCard = null;
