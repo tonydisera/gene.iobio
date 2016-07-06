105dd97d2bbe2cf20fe22360ae95af5de253d6c6
tonydisera <tony.disera@gmail.com>
2016-07-06 16:29:10 -0600

    Isolate global variables for local deployment into separate file.

--- app/globals.js
+++ app/globals.js
@@ -3,17 +3,12 @@
 * a simplified interface and logic.  For running one of the special educational edition 
 * tours (e.g. a guided tour of the gene.iobio app), turn on both isLevelEdu and isLevelEduTour.
 */
-var isOffline               = false;  // is there any internet connect to outside services and resources?
-var isClinvarOffline        = true; // is clinvar offline?  (Pull from clinvar hosted from URL?)
-
 var isLevelEdu              = false; // is gene.iobio educational version, simplified version of app
 var isLevelEduTour          = false; // is gene.iobio exhibit version, a simplified version of the app w/ guided tour
 var hideNextButtonAnim      = true;  // is next button hidden on animations during edu tour?
 
 var hasTimeout              = false; // is a timeout based on n seconds of inactivity used?
 var keepLocalStorage        = false; // maintain cache between sessions?
-
-var useDevkit               = false; // point to new minion services and use new iobio.js devkit?
 
 var eduTourNumber           = "0";
 var eduTourShowPhenolyzer   = [true, false];
@@ -42,12 +37,10 @@ var idlePrompting = false;  // prompt user to continue or just automatically res
 var stage_iobio_services    = "nv-green.iobio.io/";
 var dev_iobio_services      = "nv-dev.iobio.io/";
 var prod_iobio_services     = "nv-prod.iobio.io/";
-var self_contained_services = "frontend/"
 
-var new_iobio_services    = isOffline ? self_contained_services : dev_iobio_services;
-var iobio_services        = (isOffline ? "ws://" : "wss://")  + (isOffline ? self_contained_services : prod_iobio_services);
-
-var iobio_http_services   = "http://" + (isOffline ? self_contained_services : prod_iobio_services);
+var new_iobio_services    = isOffline ? serverInstance : dev_iobio_services;
+var iobio_services        = (isOffline ? "ws://" : "wss://")  + (isOffline ? serverInstance : prod_iobio_services);
+var iobio_http_services   = "http://" + (isOffline ? serverInstance : prod_iobio_services);
 
 var geneiobio_server     = iobio_http_services + "geneinfo/";
 var geneToPhenoServer    = iobio_http_services + "gene2pheno/";
@@ -55,8 +48,8 @@ var hpoServer            = iobio_http_services + "hpo/";
 var phenolyzerServer     = "https://7z68tjgpw4.execute-api.us-east-1.amazonaws.com/dev/phenolyzer/";
 var phenolyzerOnlyServer = iobio_http_services + "phenolyzer/";
 
-var OFFLINE_PHENOLYZER_CACHE_URL = isOffline ?  "exhibit_cache/" : "../exhibit_cache/";
-var OFFLINE_CLINVAR_VCF_URL      = isOffline ?  "http://frontend/exhibit_cache/clinvar.vcf.gz" : "https://s3.amazonaws.com/iobio/gene/clinvar/clinvar.vcf.gz";
+var OFFLINE_PHENOLYZER_CACHE_URL = isOffline ?  (serverCacheDir) : ("../" + serverCacheDir);
+var OFFLINE_CLINVAR_VCF_URL      = isOffline ?  ("http://" + serverInstance + serverCacheDir + "clinvar.vcf.gz") : "https://s3.amazonaws.com/iobio/gene/clinvar/clinvar.vcf.gz";
 
 var EXHIBIT_URL              = 'exhibit.html'
 var EXHIBIT_URL1             = 'exhibit-case-complete.html'
---
+++ app/globalsDeployment.js
@@ -0,0 +1,9 @@
+/*  These global variables determine which iobio servers the gene.iobio app with interact
+    with for a local deployment.  This entire .js can be replaced or modified to suit the 
+    specific iobio deployment environment.
+*/
+var isOffline         = false;          // is there any internet connect to outside services and resources?
+var isClinvarOffline  = true;           // is clinvar offline?  (Pull from clinvar hosted from URL?)
+var useDevkit         = true;           // point to new minion services and use new iobio.js devkit?
+var serverInstance    = "@hostname@/";  // this will be replace with the name of the server used for this deployement
+var serverCacheDir    = "local_cache/"; // this is the directory from the server instance where resource files (like clinvar vcf) will be served
