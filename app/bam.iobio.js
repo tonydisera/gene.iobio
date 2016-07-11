f9084f59c97ad447a4c2756d71f4c701dad27b1c
tonydisera <tony.disera@gmail.com>
2016-07-11 12:55:20 -0600

    Fix typo.

--- app/bam.iobio.js
+++ app/bam.iobio.js
@@ -511,7 +511,7 @@ var Bam = Class.extend({
             // New local file streaming
             stream.on('createClientConnection', function(connection) {
               var ended = 0;
-              var dataClient = BinaryClient('ws://' + (isOffline ? samtools : connection.serverAddress));
+              var dataClient = BinaryClient('ws://' + (isOffline ? me.iobio.samtools : connection.serverAddress));
               dataClient.on('open', function() {
                 var dataStream = dataClient.createStream({event:'clientConnected', 'connectionID' : connection.id});
                 dataStream.write(me.header.toStr);
