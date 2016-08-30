3a2df3b718de1e02a726ca5438c2bb46d7a7c6c2
Nielson Phu <nsnphu@gmail.com>
2016-08-30 17:19:49 -0400

    Test that choosing a gene shows the correct gene name.

--- e2e/page_objects/index.js
+++ e2e/page_objects/index.js
@@ -2,6 +2,11 @@ module.exports = {
   url: function() {
     return this.api.launchUrl;
   },
+  elements: {
+    sliderLeft: '#slider-left',
+    matrixTrack: '#matrix-track'
+  },
+
   sections: {
 
     sliderIconBar: {
@@ -25,6 +30,40 @@ module.exports = {
         examineCard: { selector: '#examine-card' },
         helpCard: { selector: '#help-card' },
         recallCard: { selector: '#recall-card' }
+      }
+    },
+
+    appTitleSection: {
+      selector: '#app-title-section',
+      elements: {
+        enterGeneName: { selector: '#enter-gene-name' },
+        addDataButton: { selector: '#add-data-button' },
+        geneBadgeName: { selector: '#gene-badge-name' }
+      }
+    },
+
+    dataCard: {
+      selector: '#data-card',
+      elements: {
+        singleProbandButton: { selector: '#single-proband-button' },
+        trioButton: { selector: '#trio-button' },
+        pedigreeButton: { selector: '#pedigree-button' },
+        otherButton: { selector: '#other-button' },
+        enterName: { selector: '#datasource-name' },
+        variantsButton: { selector: '#vcf-dropdown-button' },
+        alignmentsButton: { selector: '#bam-dropdown-button' },
+        vcfInput: { selector: '#url-input' },
+        bamInput: { selector: '#bam-url-input' },
+        loadButton: { selector: '#ok-button' },
+        platinumTrio: { selector: '#display-platinum-vcf-url-item' },
+        probandVcfSampleBox: { selector: '#proband-data #vcf-sample-box' }
+      }
+    },
+
+    matrixTrack: {
+      selector: '#matrix-track',
+      elements: {
+
       }
     }
 
--- e2e/test_e2e_spec.js
+++ e2e/test_e2e_spec.js
@@ -8,15 +8,24 @@ module.exports = {
     client.end();
   },
 
-  'Clicking the icons brings up the correct left slider': function(client) {
-  	var indexPage = client.page.index();
-  	indexPage.navigate()
-  					 .expect.section('@sliderIconBar').to.be.visible;
-  	// var iconBarSection = indexPage.section.sliderIconBar;
+  'Selecting a gene shows the correct gene name': function(client) {
+    var indexPage = client.page.index();
+    var appTitleSection = indexPage.section.appTitleSection;
+    var dataCard = indexPage.section.dataCard;
 
-  	client.end();
+    indexPage.navigate().waitForElementVisible('@sliderLeft');
+    appTitleSection.click('@addDataButton');
+    dataCard.click('@singleProbandButton')
+            .click('@variantsButton')
+            .click('@platinumTrio')
+            .waitForElementVisible('@probandVcfSampleBox')
+            .click('@loadButton');
 
+    appTitleSection.setValue('@enterGeneName', ['BRCA2', client.Keys.ARROW_DOWN, client.Keys.ENTER]);
+    indexPage.waitForElementVisible('@matrixTrack');
+    appTitleSection.expect.element('@geneBadgeName').text.to.equal('BRCA2');
 
-  },
+    client.end();
+  }
 
 };
