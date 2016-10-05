6baa66f391890cbcb458fa1a0cf9138f5468eb3d
tonydisera <tony.disera@gmail.com>
2016-10-04 21:22:46 -0600

    #397 MyGene2 basic - fix legend tooltip widths.

--- app/legend.js
+++ app/legend.js
@@ -1,14 +1,14 @@
 function Legend() {
 	this.legendMap = {
-		exon:     'The segment of DNA in the gene containing information coding for a protein',
-		HIGH:     'A variant having a high (disruptive) impact in the protien.',
-		MODERATE: 'A non-disruptive variant that might change the protein effectiveness.',
-		MODIFIER: 'Usually non-coding variants or variants affecting non-coding genes, where predictions are difficult or there is no evidence of impact.',
-		LOW:      'Assumed to be mostly harmless or unlikely to change protein behavior.',
-		snp:      'A single nucleotide change.  Multiple nucleotide changes (e.g. AT->GG) are also also identified by the square symbol',
-		ins:      'An insertion of a nucleotide(s)',
-		del:      'A deletion of nucleotide(s)',
-		complex:  'A complex variant, usually involving a combination of insertions and deletions of nucleotides'
+		exon:     {width: 190, tooltip: 'The segment of DNA in the gene containing information coding for a protein'},
+		HIGH:     {width: 170, tooltip: 'A variant having a high (disruptive) impact in the protein.'},
+		MODERATE: {width: 170, tooltip: 'A non-disruptive variant that might change the protein effectiveness.'},
+		MODIFIER: {width: 240, tooltip: 'Usually non-coding variants or variants affecting non-coding genes, where predictions are difficult or there is no evidence of impact.'},
+		LOW:      {width: 170, tooltip: 'Assumed to be mostly harmless or unlikely to change protein behavior.'},
+		snp:      {width: 240, tooltip: 'A single nucleotide change.  Multiple nucleotide changes (e.g. AT->GG) are also also identified by the square symbol'},
+		ins:      {width: 210, tooltip: 'An insertion of a nucleotide(s)'},
+		del:      {width: 190, tooltip: 'A deletion of nucleotide(s)'},
+		complex:  {width: 240, tooltip: 'A complex variant, usually involving a combination of insertions and deletions of nucleotides'}
 	}
 }
 
@@ -30,7 +30,12 @@ Legend.prototype.showLegendTooltip = function(legendId, screenX, screenY) {
 	var me = this;
 	var tooltip = d3.select('#legend-tooltip');
 
-	var tooltipText = this.legendMap.hasOwnProperty(legendId) ? this.legendMap[legendId] : legendId;
+	var legendObject =  this.legendMap[legendId];
+	if (legendObject == null) {
+		return;
+	}
+
+	var tooltipText = legendObject.tooltip;
 
 	tooltip.style("z-index", 9999);
 	tooltip.transition()
@@ -40,15 +45,16 @@ Legend.prototype.showLegendTooltip = function(legendId, screenX, screenY) {
 
 	tooltip.html(tooltipText);
 	var h = tooltip[0][0].clientHeight;
-	var w = tooltip[0][0].clientWidth;
+	var w = legendObject.width;
 
-	var x = screenX ;
-	var y = screenY + 20;
+
+	var x = screenX - 10;
+	var y = screenY - 10;
 
 	tooltip.style("width", w + "px")
 		       .style("left", x + "px")
 		       .style("text-align", 'left')
-		       .style("top", (y) + "px")
+		       .style("top", (y - h) + "px")
 		       .style("overflow-y", "hidden");
 
 }
