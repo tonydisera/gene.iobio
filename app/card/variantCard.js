451ff5dcfab952db40f81fe213b21277f18e7e76
tonydisera <tony.disera@gmail.com>
2016-10-14 16:49:51 -0600

    #469 Emergency bug fix for grant proposal.  Fixed following bug: after calling variants with freebayes - All variant marked as 'FLAGGED FOR NOT MEETING FILTERING CRITERIA'

--- app/card/variantCard.js
+++ app/card/variantCard.js
@@ -2560,7 +2560,9 @@ VariantCard.prototype.variantDetailHTML = function(variant, pinMessage, type) {
 	var qualityWarningRow = "";
 	if (filterCard.shouldWarnForNonPassVariants()) {
 		if (variant.recfilter != 'PASS') {
-			qualityWarningRow = me._tooltipLowQualityHeaderRow();
+			if (!variant.hasOwnProperty('fbCalled') || variant.fbCalled != 'Y') {
+				qualityWarningRow = me._tooltipLowQualityHeaderRow();
+			}
 		}
 	}
 
