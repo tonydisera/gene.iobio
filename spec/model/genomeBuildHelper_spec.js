0ffd66d8cd2d7781cc2fd3675301fce86c6a82e0
tonydisera <tony.disera@gmail.com>
2016-11-08 15:46:30 -0700

    #226 genomebuild - More unit tests.

--- spec/model/genomeBuildHelper_spec.js
+++ spec/model/genomeBuildHelper_spec.js
@@ -13,8 +13,19 @@ describe('genomeBuildHelper', function() {
 		{"id":14,"idSpecies":7,"name":"GRCh38",
 			"references":[{"id":313,"idGenomeBuild":14,"name":"1","length":248956422,"alias":"chr1","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.1.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr1.fa"},{"id":314,"idGenomeBuild":14,"name":"2","length":242193529,"alias":"chr2","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.2.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr2.fa"},{"id":315,"idGenomeBuild":14,"name":"3","length":198295559,"alias":"chr3","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.3.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr3.fa"},{"id":316,"idGenomeBuild":14,"name":"4","length":190214555,"alias":"chr4","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.4.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr4.fa"},{"id":317,"idGenomeBuild":14,"name":"5","length":181538259,"alias":"chr5","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.5.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr5.fa"},{"id":318,"idGenomeBuild":14,"name":"6","length":170805979,"alias":"chr6","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.6.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr6.fa"},{"id":319,"idGenomeBuild":14,"name":"7","length":159345973,"alias":"chr7","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.7.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr7.fa"},{"id":320,"idGenomeBuild":14,"name":"8","length":145138636,"alias":"chr8","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.8.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr8.fa"},{"id":321,"idGenomeBuild":14,"name":"9","length":138394717,"alias":"chr9","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.9.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr9.fa"},{"id":322,"idGenomeBuild":14,"name":"10","length":133797422,"alias":"chr10","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.10.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr10.fa"},{"id":323,"idGenomeBuild":14,"name":"11","length":135086622,"alias":"chr11","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.11.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr11.fa"},{"id":324,"idGenomeBuild":14,"name":"12","length":133275309,"alias":"chr12","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.12.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr12.fa"},{"id":325,"idGenomeBuild":14,"name":"13","length":114364328,"alias":"chr13","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.13.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr13.fa"},{"id":326,"idGenomeBuild":14,"name":"14","length":107043718,"alias":"chr14","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.14.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr14.fa"},{"id":327,"idGenomeBuild":14,"name":"15","length":101991189,"alias":"chr15","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.15.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr15.fa"},{"id":328,"idGenomeBuild":14,"name":"16","length":90338345,"alias":"chr16","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.16.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr16.fa"},{"id":329,"idGenomeBuild":14,"name":"17","length":83257441,"alias":"chr17","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.17.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr17.fa"},{"id":330,"idGenomeBuild":14,"name":"18","length":80373285,"alias":"chr18","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.18.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr18.fa"},{"id":331,"idGenomeBuild":14,"name":"19","length":58617616,"alias":"chr19","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.19.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr19.fa"},{"id":332,"idGenomeBuild":14,"name":"20","length":64444167,"alias":"chr20","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.20.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr20.fa"},{"id":333,"idGenomeBuild":14,"name":"21","length":46709983,"alias":"chr21","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.21.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr21.fa"},{"id":334,"idGenomeBuild":14,"name":"22","length":50818468,"alias":"chr22","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.22.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chr22.fa"},{"id":335,"idGenomeBuild":14,"name":"X","length":156040895,"alias":"chrX","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.X.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chrX.fa"},{"id":336,"idGenomeBuild":14,"name":"Y","length":57227415,"alias":"chrY","fastaPathEnsembl":"./data/references/homo_sapiens/GRCh38/Homo_sapiens.GRCh38.dna.chromosome.Y.fa","fastaPathUCSC":"./data/references/homo_sapiens/hg38/chrY.fa"}],
 			"resources":[{"id":31,"idGenomeBuild":14,"type":"CLINVAR VCF S3","resource":"https://s3.amazonaws.com/iobio/gene/clinvar/clinvar.GRCh38.vcf.gz"},{"id":32,"idGenomeBuild":14,"type":"CLINVAR VCF OFFLINE","resource":"clinvar.GRCh38.vcf.gz"},{"id":33,"idGenomeBuild":14,"type":"CLINVAR EUTILS BASE POSITION","resource":"Base+Position"},{"id":34,"idGenomeBuild":14,"type":"ENSEMBL URL","resource":"http://uswest.ensembl.org/Homo_sapiens/"}],
-			"aliases":[{"id":32,"idGenomeBuild":14,"type":"","alias":"NCBI38"},{"id":33,"idGenomeBuild":14,"type":"UCSC","alias":"hg38"},{"id":34,"idGenomeBuild":14,"type":"CLINVAR","alias":"c38"}]}]
+			"aliases":[{"id":32,"idGenomeBuild":14,"type":"","alias":"NCBI38"},{"id":33,"idGenomeBuild":14,"type":"UCSC","alias":"hg38"},{"id":34,"idGenomeBuild":14,"type":"CLINVAR","alias":"c38"}]
 		}
+		]
+	},
+	{"id":8,"name":"Mouse","binomialName":"Mus musculus","latin_name":"mus_musculus","genomeBuilds":[
+		{"id":13,"idSpecies":8,"name":"mm10",
+			"references":[{}],
+			"resources": [],
+			"aliases": []
+		}
+		]
+	}
+
 	];
 
 
@@ -30,6 +41,13 @@ describe('genomeBuildHelper', function() {
 			expect(genomeBuildHelper.getCurrentBuildName()).toEqual("GRCh37");
 			expect(genomeBuildHelper.getCurrentSpeciesName()).toEqual("Human");
 			expect(genomeBuildHelper.getCurrentSpeciesLatinName()).toEqual("homo_sapiens");
+
+			genomeBuildHelper.setCurrentSpecies("Mouse");
+			genomeBuildHelper.setCurrentBuild("mm10");
+			expect(genomeBuildHelper.getCurrentBuildName()).toEqual("mm10");
+			expect(genomeBuildHelper.getCurrentSpeciesName()).toEqual("Mouse");
+			expect(genomeBuildHelper.getCurrentSpeciesLatinName()).toEqual("mus_musculus");
+
 		});
 	});
 
@@ -189,10 +207,76 @@ describe('genomeBuildHelper', function() {
 			expect(speciesBuild.species.name).toEqual("Human");
 			expect(speciesBuild.build.name).toEqual("GRCh37");
 
+			var bamHeader = "@HD	VN:1.3	SO:coordinate\n"
+							+ "@SQ	SN:1	LN:248956422	SP:Human	AS:NCBI38\n"
+							+ "@SQ	SN:2	LN:242193529	SP:Human	AS:NCBI38";
+			var buildInfo = genomeBuildHelper.getBuildFromBamHeader(bamHeader);
+			expect(Object.keys(buildInfo.references).length).toEqual(2);
+			expect(buildInfo.references["1"]).toEqual(248956423);
+			expect(buildInfo.references["2"]).toEqual(242193530);
+			var speciesBuild = genomeBuildHelper.getProperSpeciesAndBuild(buildInfo);
+			expect(speciesBuild.species.name).toEqual("Human");
+			expect(speciesBuild.build.name).toEqual("GRCh38");
+
+
+			var bamHeader = "@HD	VN:1.3	SO:coordinate\n"
+							+ "@SQ	SN:1	AS:NCBI38\n"
+							+ "@SQ	SN:2	AS:NCBI38";
+			var buildInfo = genomeBuildHelper.getBuildFromBamHeader(bamHeader);
+			var speciesBuild = genomeBuildHelper.getProperSpeciesAndBuild(buildInfo);
+			expect(speciesBuild.species.name).toEqual("Human");
+			expect(speciesBuild.build.name).toEqual("GRCh38");
+
 
 		})
 	});
 
+	describe('#getBuildsInHeaders', function() {
 
+		it('get the build infos from the headers of vcf and bams', function() {
+			var bamHeader = "@HD	VN:1.3	SO:coordinate\n"
+							+ "@SQ	SN:1	LN:249250621\n"
+							+ "@SQ	SN:2	LN:243199373\n"
+							+ "@SQ	SN:3	LN:198022430\n"
+							+ "@SQ	SN:4	LN:191154276\n"
+							+ "@SQ	SN:5	LN:180915260\n"
+							+ "@SQ	SN:6	LN:171115067\n"
+							+ "@SQ	SN:X	LN:155270560\n"
+							+ "@SQ	SN:Y	LN:59373566";
+
+			var vcfHeader = "##contig=<ID=1,length=249250621>\n##contig=<ID=chr2,length=243199373>";
+
+			var speciesBuilds = genomeBuildHelper.getBuildsInHeaders({proband: bamHeader}, {mother: vcfHeader});
+			expect(speciesBuilds.length).toEqual(1);
+			expect(speciesBuilds[0].species.name).toEqual('Human');
+			expect(speciesBuilds[0].build.name).toEqual('GRCh37');
+			expect(speciesBuilds[0].from.length).toEqual(2);
+			expect(speciesBuilds[0].from[0].relationship).toEqual('proband');
+			expect(speciesBuilds[0].from[0].type).toEqual('bam');
+			expect(speciesBuilds[0].from[1].relationship).toEqual('mother');
+			expect(speciesBuilds[0].from[1].type).toEqual('vcf');
+
+
+			var bamHeaderMother = "@HD	VN:1.3	SO:coordinate\n"
+						    	+ "@SQ	SN:1	AS:NCBI38\n"
+						    	+ "@SQ	SN:2	AS:NCBI38";
+			speciesBuilds = genomeBuildHelper.getBuildsInHeaders({proband: bamHeader, mother: bamHeaderMother}, {mother: vcfHeader});
+			expect(speciesBuilds.length).toEqual(2);
+			expect(speciesBuilds[0].species.name).toEqual('Human');
+			expect(speciesBuilds[0].build.name).toEqual('GRCh37');
+			expect(speciesBuilds[0].from.length).toEqual(2);
+			expect(speciesBuilds[0].from[0].relationship).toEqual('proband');
+			expect(speciesBuilds[0].from[0].type).toEqual('bam');
+			expect(speciesBuilds[0].from[1].relationship).toEqual('mother');
+			expect(speciesBuilds[0].from[1].type).toEqual('vcf');
+
+			expect(speciesBuilds[1].species.name).toEqual('Human');
+			expect(speciesBuilds[1].build.name).toEqual('GRCh38');
+			expect(speciesBuilds[1].from.length).toEqual(1);
+			expect(speciesBuilds[1].from[0].relationship).toEqual('mother');
+			expect(speciesBuilds[1].from[0].type).toEqual('bam');
+
+		});
+	});
 
 });
