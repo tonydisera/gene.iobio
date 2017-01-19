tonydisera/gene.iobio

commit b56153b15ee8852b7e82dcbd85ecef2e7d549894

#513 - After initial set of genes analyzed with ‘Analyze all’, newly added genes were not included in the total count and the analyze all would re-analyze the genes already analyzed.  Also fixed bug that was throwing off total genes analyzed after gene removed.

app/model/variantTrioModel.js
@@ -140,7 +140,7 @@ VariantTrioModel.prototype.compareVariantsToMotherFather = function(callback) {
 			} 
 		});
 
-		getProbandVariantCard().setLoadState('inheritance');
+		me.probandVcfData.loadState['inheritance'] = true;
 		filterCard.enableInheritanceFilters(me.probandVcfData);
 
 		callback(me.probandVcfData);

app/card/genesCard.js
@@ -194,7 +194,11 @@ GenesCard.prototype.sortGenes = function(sortBy) {
 }
 
 GenesCard.prototype.getGeneNames = function() {
-	return this.sortedGeneNames || window.geneNames;
+	if (this.sortedGeneNames && this.sortedGeneNames.length == window.geneNames.length) {
+		return sortedGeneNames;		
+	} else {
+		return window.geneNames;
+	}
 }
 
 GenesCard.prototype.pageNumberForGene = function(geneName) {
@@ -993,6 +997,7 @@ GenesCard.prototype.removeGeneBadgeByName = function(theGeneName) {
 		gb.remove();
 		me._onGeneBadgeUpdate();
 	}
+	cacheHelper.clearCacheForGene(theGeneName);
 	delete geneObjects[theGeneName];
 	delete geneAnnots[theGeneName];
 	delete geneUserVisits[theGeneName];
@@ -1027,6 +1032,7 @@ GenesCard.prototype._clearGenesImpl = function() {
 		var gb = me._getGeneBadge(theGeneName);
 		gb.remove();
 
+		cacheHelper.clearCacheForGene(theGeneName);
 		delete geneObjects[theGeneName];
 		delete geneAnnots[theGeneName];
 		delete geneUserVisits[theGeneName];
@@ -1049,6 +1055,7 @@ GenesCard.prototype.removeGeneBadge = function(badgeElement) {
 
 		me._onGeneBadgeUpdate();
 	}
+	cacheHelper.clearCacheForGene(theGeneName);
 	delete geneObjects[theGeneName];
 	delete geneAnnots[theGeneName];
 	delete geneUserVisits[theGeneName];

app/model/cacheHelper.js
@@ -119,7 +119,7 @@ CacheHelper.prototype.cacheGene = function(geneName, callback) {
     	window.geneObjects[geneObject.gene_name] = geneObject;
 
     	isJointCallOnly( function(shouldJointCall) {
-		    if (me.isCachedForCards(geneObject.gene_name, transcript)) {
+		    if (me.isCachedForProband(geneObject.gene_name, transcript)) {
 		    	// take this gene off of the queue and see
 		    	// if next batch of genes should be analyzed
 		    	genesCard._geneBadgeLoading(geneObject.gene_name, false);
@@ -274,6 +274,10 @@ CacheHelper.prototype.cacheNextGene = function(geneName, callback) {
 	// of genes to analyze once all of the genes in
 	// the current batch have been analyzed.
 	this.cacheGenes(callback);
+}
+
+CacheHelper.prototype.isCachedForProband = function(geneName, transcript) {
+	return getProbandVariantCard().model.isCachedAndInheritanceDetermined(geneName, transcript); 
 }
 
 CacheHelper.prototype.isCachedForCards = function(geneName, transcript) {
@@ -598,7 +602,7 @@ CacheHelper.prototype.openDialog = function() {
 
 CacheHelper.prototype.clearCacheForGene = function(geneName) {
 	var me = this;
-	var keys = me.getKeysForGene(geneName);
+	var keys = me._getKeysForGene(geneName);
 	keys.forEach( function(key) {
 		localStorage[key] = "";
 	});

app/model/variantModel.js
@@ -1055,6 +1055,11 @@ VariantModel.prototype.isCached = function(geneName, transcript) {
 	return data != null;
 }
 
+VariantModel.prototype.isCachedAndInheritanceDetermined = function(geneName, transcript) {
+	var theVcfData = this._getCachedData("vcfData", geneName, transcript);
+	return theVcfData && theVcfData.loadState != null && theVcfData.loadState['inheritance'];
+}
+
 VariantModel.prototype.promiseCacheVariants = function(ref, geneObject, transcript) {
 	var me = this;
 
@@ -1637,7 +1642,7 @@ VariantModel.prototype._refreshVariantsWithCoverage = function(theVcfData, cover
 		}
 
 	}
-	this.setLoadState(theVcfData, 'coverage');
+	theVcfData.loadState['coverage'] = true;
 	callback();
 
 

commit 61f0f1f281418c4da51093dbd55ab18587ff4415

#513 - Fix bug introduced with commit b56153b

app/model/variantTrioModel.js
@@ -140,7 +140,12 @@ VariantTrioModel.prototype.compareVariantsToMotherFather = function(callback) {
 			} 
 		});
 
-		me.probandVcfData.loadState['inheritance'] = true;
+		if (me.probandVcfData) {
+			if (!me.probandVcfData.loadState) {
+				me.probandVcfData.loadState = {};
+			}
+			me.probandVcfData.loadState['inheritance'] = true;
+		}
 		filterCard.enableInheritanceFilters(me.probandVcfData);
 
 		callback(me.probandVcfData);