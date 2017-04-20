762d35224aa74285a1368a6e977c1d89a23ced71
tonydisera <tony.disera@gmail.com>
2017-04-20 13:44:28 -0600

    #585 - Write more selenium tests for bookmarking variants and beef up MatrixTooltip page object to help with other UI tests.

--- e2e/page_objects/sections/matrixTooltip.js
+++ e2e/page_objects/sections/matrixTooltip.js
@@ -33,6 +33,116 @@ module.exports = {
       })
     },
 
+
+    expectInheritanceEquals: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//*[local-name()="svg" and @class="inheritance-badge"]/following-sibling::strong/em';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },
+    expectVepConsequence: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="VEP Consequence"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },  
+    expectVepImpact: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Impact"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },     
+    expectHighestVepImpact: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Most severe impact"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    }, 
+    expectPolyphen: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="PolyPhen"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },         
+    expectSIFT: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="SIFT"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },         
+    expectClinvarEquals: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="ClinVar"]/following-sibling::div/span/a';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },
+    expectClinvarClinSigEquals: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="ClinVar"]/parent::node()/following-sibling::div/div[@class="tooltip-value"]';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },  
+    expectAFExAC: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Allele Freq ExAC"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },   
+    expectAF1000G: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Allele Freq 1000G"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },  
+    expectQual: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Qual"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    }, 
+    expectFilter: function(text) {
+      var self = this;
+      var selector = '//div[@id="feature-matrix"]/div[contains(@class,"tooltip") and contains(@class,"tooltip-wide")]//div[@class="tooltip-header" and text()="Filter"]/following-sibling::div';
+      return self.api.useXpath().getText(selector, function(result) {
+        self.assert.equal(result.value, text);
+      })
+    },              
+    expectAlleleCountsEquals: function(relationship, altCount, refCount, totalCount, zygosity) {
+      var self = this;
+      var base       = '//div[@id="feature-matrix"]//div[contains(@class, "tooltip")]//div[@id="coverage-svg"]/div[contains(@class,"' + relationship + '-alt-count") and contains(@class,"tooltip-row")]';
+      var zygosityPath   = base + '/div[contains(@class,"tooltip-zygosity")]';
+      var totalCountPath = base + '/div[contains(@class,"tooltip-allele-count-bar")]/*[local-name()="svg"]/*[local-name()="text"]';
+      var altCountPath   = base + '/div[contains(@class,"tooltip-allele-count-bar")]/*[local-name()="svg"]/*[local-name()="g"]/*[local-name()="text" and contains(@class, "alt-count")]';
+      var refCountPath   = base + '/div[contains(@class,"tooltip-allele-count-bar")]/*[local-name()="svg"]/*[local-name()="g"]/*[local-name()="text" and contains(@class, "ref-count")]';
+      self.api.useXpath().getText(zygosityPath, function(result) {
+        self.assert.equal(result.value, zygosity);
+      })
+      self.api.useXpath().getText(totalCountPath, function(result) {
+        self.assert.equal(result.value, totalCount);
+      })
+      if (altCount) {
+        self.api.useXpath().getText(altCountPath, function(result) {
+          self.assert.equal(result.value, altCount);
+        })        
+      }
+      if (refCount) {
+        self.api.useXpath().getText(refCountPath, function(result) {
+          self.assert.equal(result.value, refCount);
+        })        
+      }
+    }, 
+
     clickBookmark: function() {
       return this.click('@bookmarkLink');
     },
--- e2e/spec/bookmark_variants.js
+++ e2e/spec/bookmark_variants.js
@@ -29,6 +29,20 @@ module.exports = {
   'Bookmark 2 variants for gene RAI1 from matrix tooltip': function(client) {
     matrixTrack.clickColumn(1);
     matrixTooltip.waitForTooltip();
+
+    matrixTooltip.expectInheritanceEquals('recessive inheritance');
+    matrixTooltip.expectVepImpact('high');
+    matrixTooltip.expectVepConsequence('stop gained');
+    matrixTooltip.expectClinvarEquals('pathogenic');
+    matrixTooltip.expectClinvarClinSigEquals('smith-magenis syndrome');
+    matrixTooltip.expectAFExAC('0%');
+    matrixTooltip.expectAF1000G('0%');
+    matrixTooltip.expectQual('2880.99');
+    matrixTooltip.expectFilter('.');
+    matrixTooltip.expectAlleleCountsEquals('proband', 38, 1,  39, 'Hom');
+    matrixTooltip.expectAlleleCountsEquals('mother',  26, 25, 51, 'Het');
+    matrixTooltip.expectAlleleCountsEquals('father',  30, 33, 63, 'Het');
+
     matrixTooltip.clickBookmark();
     client.pause(1000);
     bookmarkPanel.assertCurrentBookmarkCoordEquals('17698535 G->A');
@@ -42,26 +56,45 @@ module.exports = {
     bookmarkPanel.assertBookmarkCountEquals(2);
   },
     
+    
   'Click on another gene and bookmark a third variant from tooltip': function(client) {
     nav.searchGene('PDHA1');
     client.pause(1000);
     matrixTrack.waitForMatrixLoaded();
     matrixTrack.clickColumn(1);
     matrixTooltip.waitForTooltip();
+
+
+    matrixTooltip.expectInheritanceEquals('denovo inheritance');
+    matrixTooltip.expectVepImpact('moderate');
+    matrixTooltip.expectVepConsequence('missense variant');
+    matrixTooltip.expectPolyphen('probably damaging');
+    matrixTooltip.expectSIFT('deleterious');
+    matrixTooltip.expectAlleleCountsEquals('proband', 27, 76, 103, 'Het');
+    matrixTooltip.expectAlleleCountsEquals('mother',  null, null, 13, 'Homref');
+    matrixTooltip.expectAlleleCountsEquals('father',  null, null, 42, 'Homref');
+
+
     matrixTooltip.clickBookmark();
     client.pause(1000);
+
+    
     bookmarkPanel.assertCurrentBookmarkCoordEquals('19369471 G->T');
     bookmarkPanel.assertCurrentBookmarkRsIdEquals('');
     bookmarkPanel.assertCurrentBookmarkHgvsEquals('p.Gly160Cys');
     bookmarkPanel.assertBookmarkCountEquals(3);
   },
 
+
   'Click on a bookmark link in the bookmark panel and show matrix tooltip': function(client) {
+    client.pause(2000);
     bookmarkPanel.clickBookmark(client, "17698535 G->A");
     matrixTrack.waitForMatrixLoaded();
     matrixTooltip.waitForTooltip();
+    client.pause(2000);
     matrixTooltip.expectTitleLine2TextEquals('SNP G->A rs527236033');
     matrixTooltip.expectHGVSpTextEquals('ENSP00000323074.4:p.Trp758Ter');
+    matrixTooltip.expectAlleleCountsEquals('proband', 38, 1, 39, 'Hom');
 
   },
 
