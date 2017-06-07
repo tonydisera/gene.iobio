531be61c65dfbcbd360ac0e62b66a722f6d640c8
tonydisera <tony.disera@gmail.com>
2017-06-06 23:02:04 -0600

    Fix WebWorker code to be excluded from Jasmine unit test.

--- app/model/cacheHelperWorker.js
+++ app/model/cacheHelperWorker.js
@@ -1,23 +1,27 @@
-self.addEventListener('message', function(e) {
-  var data = e.data;
-  importScripts('../../assets/js/lz-string.min.js'); 
+if( 'function' === typeof importScripts) {
 
-  switch (data.cmd) {
-    case 'start':
-      var dataString = null;
-      try {
-         dataString = LZString.decompressFromUTF16(data.compressedData);
-         var uncompressedData =  JSON.parse(dataString);      
-         postMessage(uncompressedData);    
-      } catch(e) {
-          
-      }
-      break;
-    case 'stop':
-      self.postMessage('stopped');
-      self.close(); // Terminates the worker.
-      break;
-    default:
-      self.postMessage('unknown');
-  };
-}, false);
+  self.addEventListener('message', function(e) {
+    var data = e.data;
+    importScripts('../../assets/js/lz-string.min.js'); 
+
+    switch (data.cmd) {
+      case 'start':
+        var dataString = null;
+        try {
+           dataString = LZString.decompressFromUTF16(data.compressedData);
+           var uncompressedData =  JSON.parse(dataString);      
+           postMessage(uncompressedData);    
+        } catch(e) {
+            
+        }
+        break;
+      case 'stop':
+        self.postMessage('stopped');
+        self.close(); // Terminates the worker.
+        break;
+      default:
+        self.postMessage('unknown');
+    };
+  }, false);
+
+}
